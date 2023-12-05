69c4f3167e8845ffb85dcdf1e2a1099c4e1d9824
lorenz369 <lorenz369@outlook.de>
2023-12-04 17:26:02 -0800

    Add organize.asm :wq q

--- organize.asm
+++ organize.asm
@@ -7,54 +7,62 @@ organize:
 
     # function body void organize(){
     li t0, 0    # short val;
-    li s0, 0    # short val_inter;
-    li t1, 1    # uint8_t in_size = 1;
+    li t1, 1    # uint8_t x_size = 1;
     li t2, 0    # out_index = 0;
     li t3, 0    # in_index = 0;
     li t4, 0    # output_buffer = 0;
     li t5, 0    # output_size = 0;
-    while(1) {
-        val = x_val[in_index];
-        size = x_size[in_index];
-        in_index++;
-        if (size == 0) {break;}
-        if (val < 0) {
-            val -= 1;
-        }
-        output_buffer << size;
-        output_buffer += val;
-        output_size += size;
-        if (output_size >= 8) {
-            output_size -= 8;
-            offset = output_size - 8;
-            temp = output_buffer;
-            output_buffer >>= offset;
-            output[out_index] = output_buffer;
-            out_index++;
-            if output_buffer == 0xff {
-                output[out_index] = 0x00;
-                out_index++;
-                output_buffer = 0;
-                output_size = 0;
-            }
-            else {
-                output_buffer = temp & (0xFF >> (8 - offset));
-                output_size = offset;
-            }
-        }
-    }
-    if (output_size != 0) {
-        output_buffer <<= (8 - output_size);
-        output_buffer |= (0xFF >> (8 - output_size));
-        output[out_index] = output_buffer;
-        out_index++;
-    }
-
+    li s0, 0xff
+    li s1, 8
+    while:              # while(1) {
+        add t6, x10, t3 
+        lw t0, 0(t6)                # val = x_val[in_index];
+        add t6, x11, t3 
+        lw t1, 0(t6)                # x_size = x_size[in_index];
+        addi t3, t3, 4              # in_index++;
+        beqz t1, end                # if (x_size == 0) {break;}
+        li t6, -1
+        bgt t0, t6, positive_x       # if (val < 0) {
+            addi t0, t0, -1             # val -= 1;
+            sub t6, s1, t1              # t6 = 8 - x_size
+            srl t6, s0, t6
+            and t0, t0, t6              # val |= 0xFF >> (8 - x_size);}
+        positive_x:
+            sll t4, t4, t1         # output_buffer <<= x_size;
+            or t4, t4, t0           # output_buffer |= val; =+?
+            add t5, t5, t1          # output_size += x_size;
+            blt t5, s1, while       # if (output_size < 8) {continue;}
+            sub s2, t5, s1          # s2/offset = output_size - 8
+            addi t5, t5, -8         # output_size -= 8;
+            mv s3, t4               # s3/temp = output_buffer
+            srl t4, t4, s2          # output_buffer >>= offset;
+            add t6, x12, t2         # t6 = output + out_index
+            sw t4, 0(t6)            # output[out_index] = output_buffer;
+            addi t2, t2, 4          # out_index++;
+            bne t4, s0, else       # if output_buffer == 0xff {
+            add t6, x12, t2            
+            sw x0, 0(t6)                # output[out_index] = 0x00;
+            addi t2, t2, 4              # out_index++;
+            else:                   # else {
+                mv t5, s2               # output_size = offset;
+                sub s2, s1, s2
+                srl s2, s0, s2         # 0xFF >> (8 - offset);
+                and t4, s3, s2          # output_buffer = temp & (0xFF >> (8 - offset));
+                j while             # }
+    end:                    
+        beqz t5, return    # if (output_size != 0) {
+        sub t6, s1, t5          # t6 = 8 - output_size
+        sll t4, t4, t6              # output_buffer <<= (8 - output_size);
+        srl t6, s0, t5         # 0xFF >> output_size;
+        or t4, t4, t6           # output_buffer |= (0xFF >> (8 - output_size));
+        add t6, x12, t2        # t6 = output + out_index
+        sw t4, 0(t6)            # output[out_index] = output_buffer;
+        addi t2, t2, 4          # out_index++; }
     # }
-   
 
     return:
         # restore registers from stack if needed
         lw ra, 0(sp) # Restore return address register
         addi sp, sp, 4 # deallocate space for stack frame
         ret # return to calling point
+    
